de904a6631a824b278d70b1ddfc5538006a5ca62
bestwebninja <13558696+bestwebninja@users.noreply.github.com>
2026-05-19 11:20:27 +0300

    Create index.ts

---
+++ src/domain/campaign/index.ts
@@ -0,0 +1,4 @@
+export * from "./campaign.model";
+export * as CampaignRepo from "./campaign.repo";
+export * as CampaignService from "./campaign.service";
+export * as CampaignSelectors from "./campaign.selectors";
