a2976c010a9c6a8fdf530b84175529e74451615f
bestwebninja <13558696+bestwebninja@users.noreply.github.com>
2026-05-19 13:03:45 +0300

    Update index.ts

--- src/domain/campaign/index.ts
+++ src/domain/campaign/index.ts
@@ -1,4 +1,1 @@
-export * from "./campaign.model";
-export * as CampaignRepo from "./campaign.repo";
-export * as CampaignService from "./campaign.service";
-export * as CampaignSelectors from "./campaign.selectors";
+export type { Campaign } from "./campaign.types";
