76fb3067a80d0de45d7a4ef8bf9d832e0d3ad430
bestwebninja <13558696+bestwebninja@users.noreply.github.com>
2026-05-19 13:05:23 +0300

    Update campaign.types.ts

--- src/domain/campaign/campaign.types.ts
+++ src/domain/campaign/campaign.types.ts
@@ -2,14 +2,20 @@ export type Campaign = {
   id: string;
   handle: string;
   title: string;
+
   image_url?: string | null;
   short_description?: string | null;
   story?: string | null;
+
   location?: string | null;
+
   status?: "draft" | "active" | "paused" | "completed";
+
   currency: string;
   goal_amount: number;
   raised_amount: number;
+
   donor_count: number;
+
   shopify_variant_id?: string | null;
 };
